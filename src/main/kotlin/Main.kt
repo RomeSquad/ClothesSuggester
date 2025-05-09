887b5fdebc8bfa45e6b64e2f914e2f4f7836bdb4
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-05-09 17:30:41 +0300

    add manual dependency injection setup in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,43 @@
 package org.example
 
+import org.example.data.api.WeatherApiImpl
+import org.example.data.api.network.HttpClientImpl
+import org.example.data.api.network.NetworkConfig
+import org.example.data.api.network.UrlBuilder
+import org.example.data.repository.WeatherRepositoryImpl
+import org.example.data.repository.mapper.GeocodingMapper
+import org.example.data.repository.mapper.WeatherMapper
+import org.example.logic.usecase.GetWeatherUseCase
+import org.example.presentation.io.ConsoleInputReader
+import org.example.presentation.io.ConsoleWriter
+import org.example.presentation.navigation.MainUiController
+import org.example.presentation.navigation.NavigationController
+import org.example.presentation.navigation.Route
+
 
 fun main() {
+    MainUiController(
+        navigationController = NavigationController(),
+        viewer = ConsoleWriter(),
+        reader = ConsoleInputReader(),
+        getWeatherUseCase = GetWeatherUseCase(
+            weatherRepository = WeatherRepositoryImpl(
+                weatherApi = WeatherApiImpl(
+                    httpClient = HttpClientImpl(),
+                    urlBuilder = UrlBuilder(),
+                    config = NetworkConfig,
+                    weatherMapper = WeatherMapper(),
+                    geocodingMapper = GeocodingMapper()
+                ),
+                geocodingApi = WeatherApiImpl(
+                    httpClient = HttpClientImpl(),
+                    urlBuilder = UrlBuilder(),
+                    config = NetworkConfig,
+                    weatherMapper = WeatherMapper(),
+                    geocodingMapper = GeocodingMapper()
+                )
+            )
+        )
+    )
 
 }

b6da77ccb74899c03a3bd866ba20df56d6ac35a5
mohamed <mohamed.ramadan19502@gmail.com>
2025-05-09 16:55:31 +0300

    create SuggestClothingUseCase and add relevant files for it

--- src/main/kotlin/logic/usecase/SuggestClothingUseCase.kt
+++ src/main/kotlin/logic/usecase/SuggestClothingUseCase.kt
@@ -1,4 +1,29 @@
 package org.example.logic.usecase
 
-class SuggestClothingUseCase {
+import logic.entity.Weather
+import org.example.logic.repository.ClothingSuggestion
+import logic.state.ColdWeatherState
+import logic.state.FreezingWeatherState
+import logic.state.HotWeatherState
+import logic.state.HumidityWeatherState
+import logic.state.RainyWeatherState
+import logic.state.UvIndexWeatherState
+import logic.state.WarmWeatherState
+
+class SuggestClothingUseCase (
+    private val rules: List<ClothingSuggestion> = listOf(
+        ColdWeatherState(),
+        FreezingWeatherState(),
+        HotWeatherState(),
+        HumidityWeatherState(),
+        RainyWeatherState(),
+        UvIndexWeatherState(),
+        WarmWeatherState()
+    )
+) {
+    fun suggestClothing(weather: Weather): List<String> {
+        return rules
+            .filter { it.condition(weather) }
+            .flatMap { it.getClothingLists() }
+    }
 }
